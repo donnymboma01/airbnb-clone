c75605ef151f151e456bf625a674374379c9f291
donnymboma01 <82469294+donnymboma01@users.noreply.github.com>
2024-04-29 18:18:19 +0200

    feat: auth signup & signin

--- app/components/user-nav.tsx
+++ app/components/user-nav.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -9,30 +10,52 @@ import { MenuIcon } from "lucide-react";
 import {
   RegisterLink,
   LoginLink,
+  LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function UserNav() {
+export async function UserNav() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <div className="rounded-full border px-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
           <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
           <img
-            src="https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg"
+            src={
+              user?.picture ??
+              "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg"
+            }
             alt="Image of the user"
             className="rounded-full h-8 w-8 lg:block "
           />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[200px]">
-        <DropdownMenuItem>
-          {" "}
-          <RegisterLink className="w-full">Register</RegisterLink>{" "}
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          {" "}
-          <LoginLink className="w-full">Login</LoginLink>{" "}
-        </DropdownMenuItem>
+        {user ? (
+          <>
+            <DropdownMenuItem>
+              <LogoutLink className="w-full">Se déconnecter</LogoutLink>{" "}
+            </DropdownMenuItem>
+            {/* <DropdownMenuItem>
+              {" "}
+              <LoginLink className="w-full">Se connecter</LoginLink>{" "}
+            </DropdownMenuItem> */}
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem>
+              {" "}
+              <RegisterLink className="w-full">S'inscire</RegisterLink>{" "}
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              {" "}
+              <LoginLink className="w-full">Se connecter</LoginLink>{" "}
+            </DropdownMenuItem>
+          </>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
