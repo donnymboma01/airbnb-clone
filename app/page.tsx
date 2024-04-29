5dcccb0f8e5367b0dc063ccbb01a92d99a6710cc
donnymboma01 <82469294+donnymboma01@users.noreply.github.com>
2024-04-29 20:10:55 +0200

    feat: search params & categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { MapFilterItems } from "./components/mapfilteritems";
 
 export default function Home() {
   return (
-    <div>
-      <Button size="lg">Bonjour, je suis shadcn-button</Button>
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
     </div>
   );
 }
